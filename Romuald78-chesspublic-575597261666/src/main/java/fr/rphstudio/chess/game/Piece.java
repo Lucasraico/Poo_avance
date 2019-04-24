ca23a1e6c3b2caf5a5beedc73c29625ad3df67e6
begarin Rodier Freddy <begarinrodierfreddy@MacBook-Pro-de-admin.local>
2019-04-24 09:25:48 +0200

    piece début

---
+++ Romuald78-chesspublic-575597261666/src/main/java/fr/rphstudio/chess/game/Piece.java
@@ -0,0 +1,21 @@
+package fr.rphstudio.chess.game;
+
+import fr.rphstudio.chess.interf.IChess;
+
+public class Piece  {
+    private IChess.ChessColor Color;
+    private IChess.ChessType Type;
+
+    public Piece(IChess.ChessColor color, IChess.ChessType type) {
+        Color = color;
+        Type = type;
+    }
+
+    public IChess.ChessColor getColor() {
+        return Color;
+    }
+
+    public IChess.ChessType getType() {
+        return Type;
+    }
+}
