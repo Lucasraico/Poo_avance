6819fce502a50aaa437d90feb5ed5dafbf5832b9
Lucas raicovitch <lucasraicovitch@MacBook-Pro-de-Alice.local>
2019-04-24 13:59:35 +0200

    Pion fin

--- Romuald78-chesspublic-575597261666/src/main/java/fr/rphstudio/chess/game/Board.java
+++ Romuald78-chesspublic-575597261666/src/main/java/fr/rphstudio/chess/game/Board.java
@@ -7,14 +7,14 @@ public class Board {
 
     public Board() {
         this.table = new  Piece [IChess.BOARD_WIDTH][IChess.BOARD_HEIGHT];
-        table [0][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
-        table [1][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
-        table [2][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
-        table [3][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
-        table [4][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
-        table [5][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
-        table [6][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
-        table [7][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
+        table [0][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_ROOK);
+        table [1][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_KNIGHT);
+        table [2][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_BISHOP);
+        table [3][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_QUEEN);
+        table [4][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_KING);
+        table [5][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_BISHOP);
+        table [6][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_KNIGHT);
+        table [7][0] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_ROOK);
         table [0][1] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
         table [1][1] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
         table [2][1] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
@@ -32,14 +32,14 @@ public class Board {
         table [5][6] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
         table [6][6] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
         table [7][6] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
-        table [0][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
-        table [1][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
-        table [2][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
-        table [3][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
-        table [4][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
-        table [5][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
-        table [6][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
-        table [7][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
+        table [0][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_ROOK);
+        table [1][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_KNIGHT);
+        table [2][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_BISHOP);
+        table [3][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_QUEEN);
+        table [4][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_KING);
+        table [5][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_BISHOP);
+        table [6][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_KNIGHT);
+        table [7][7] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_ROOK);
     }
 
     public Piece getPieces(IChess.ChessPosition position){
